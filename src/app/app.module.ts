f93c56b2aae1cf0b58925c20b8064ea4faf36e92
tjung <taeheejung@gmail.com>
2019-06-10 10:24:45 -0700

    page2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { Page2Component } from './page2/page2.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material'
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    Page2Component
+    Page2Component,
   ],
   imports: [
     BrowserModule,
-    appRouting
+    appRouting,
+    BrowserAnimationsModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
